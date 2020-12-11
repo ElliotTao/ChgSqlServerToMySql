38efa24721333a729f25326390f27f4e6ed67f69
陶榕榕 <rongrong.tao@sharegoo.cn>
2020-12-11 17:26:31 +0800

    第一次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ChgSqlServerToMySql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SqlTextBox = new System.Windows.Forms.TextBox();
+            this.MySqlTextBox = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SqlTextBox
+            // 
+            this.SqlTextBox.Location = new System.Drawing.Point(28, 12);
+            this.SqlTextBox.Multiline = true;
+            this.SqlTextBox.Name = "SqlTextBox";
+            this.SqlTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SqlTextBox.Size = new System.Drawing.Size(740, 150);
+            this.SqlTextBox.TabIndex = 0;
+            // 
+            // MySqlTextBox
+            // 
+            this.MySqlTextBox.Location = new System.Drawing.Point(28, 251);
+            this.MySqlTextBox.Multiline = true;
+            this.MySqlTextBox.Name = "MySqlTextBox";
+            this.MySqlTextBox.Size = new System.Drawing.Size(740, 150);
+            this.MySqlTextBox.TabIndex = 1;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(218, 182);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(106, 50);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "转换";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(452, 182);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(123, 50);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "复制MySQL脚本";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.MySqlTextBox);
+            this.Controls.Add(this.SqlTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SqlTextBox;
+        private System.Windows.Forms.TextBox MySqlTextBox;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChgSqlServerToMySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sqlStr = SqlTextBox.Text;
+            var mySqlStr = GetMySqlStr(sqlStr);
+            MySqlTextBox.Text = mySqlStr;
+        }
+
+        private string GetMySqlStr(string SqlStr)
+        {
+            var mySqlStr = string.Empty;
+            if (SqlStr.Contains("--增"))
+            {
+                var sqlList = new List<string>();
+                var count = Regex.Matches(SqlStr, "--增").Count;
+                var oldIndex = 0;
+                if (count>1)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        var newIndex = SqlStr.IndexOf("--增");
+                        if (newIndex <= 0) newIndex = SqlStr.IndexOf("--增", oldIndex + 1);
+                        if (i == (count - 1))
+                        {
+                            sqlList.Add(SqlStr.Substring(oldIndex));
+                        }
+                        else
+                        {
+                            sqlList.Add(SqlStr.Substring(oldIndex, newIndex-oldIndex));
+                        }
+                        oldIndex = newIndex;
+                    }
+                }
+                else
+                {
+                    sqlList.Add(SqlStr);
+                }
+
+                foreach (var sql in sqlList)
+                {
+                    if (sql.Contains("--增"))
+                    {
+                        var str = Regex.Split(sql, "--\r\n").Where(t => t.Length > 0).ToList();
+                        mySqlStr += str[0].Replace("--增", "-- 增") + "--\r\n";
+                        mySqlStr += ChgSql2MySql(str[1]);
+                    }
+                    else
+                    {
+                        mySqlStr += ChgSql2MySql(sql);
+                    }
+                }
+            }
+            else
+            {
+                mySqlStr = ChgSql2MySql(SqlStr);
+            }
+
+            return mySqlStr;
+        }
+
+        private string ChgSql2MySql(string SqlStr)
+        {
+            var mySqlStr = string.Empty;
+            var sqlList = Regex.Split(SqlStr, "if not exists", RegexOptions.IgnoreCase).Where(t => t.Length > 0).ToList();
+            foreach (var sql in sqlList)
+            {
+                var partList = Regex.Split(sql.Replace("dbo.","").Replace("\r\n", "\n"), "\n");
+                mySqlStr += partList[1].Replace("insert ", "INSERT INTO ") + "\r\n";
+
+                var index = partList[2].LastIndexOf(')');
+                partList[2] = partList[2].Remove(index, partList[2].Length - index);
+                partList[2] = partList[2].Insert(index, "\r\nFROM DUAL WHERE NOT EXISTS " + partList[0] + ";");
+                mySqlStr += partList[2].ReplaceIngoreCase("  values(", "SELECT ").ReplaceIngoreCase("values(", "SELECT ")
+                    .ReplaceIngoreCase("values (", "SELECT ") + "\r\n";
+            }
+            return mySqlStr;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(MySqlTextBox.Text);
+        }
+
+    }
+
+    public static class replaceStr
+    {
+        public static string ReplaceIngoreCase(this string str, string OldValue, string NewValue)
+        {
+            return Regex.Replace(str, Regex.Escape(OldValue), NewValue, RegexOptions.IgnoreCase);
+        }
+    }
+}
